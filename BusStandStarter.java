d3aa7e214e591a5cf6da49711dde17cfe5903d75
AmruthaaD <amruthad.xworkz@gmail.com>
2023-10-31 12:16:50 +0530

    main method

---
+++ BusStandStarter.java
@@ -0,0 +1,21 @@
+class BusStandStarter{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting main in SpaceStation");
+		BusStand busstand= new BusStand();
+		System.out.println("Bus StandName :"+busstand.busStandName);
+		System.out.println("Parking location :"+busstand.parkingLocation);
+		System.out.println("terminal :"+busstand.terminal);
+		System.out.println("Station pincode :"+busstand.pincode);
+		System.out.println("Location :"+busstand.location);
+		BusStand busstand1=new BusStand();
+		System.out.println("bus StandName :"+busstand1.busStandName);
+		System.out.println("Parking location :"+busstand1.parkingLocation);
+		System.out.println("terminal :"+busstand1.terminal);
+		System.out.println("pincode :"+busstand1.pincode);
+		System.out.println("Location :"+busstand1.location);
+		System.out.println("Starting main in SpaceStation");
+
+	}
+	
+}
