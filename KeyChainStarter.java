1b0f6b7ce7a19d0f60c51a41c49828c1592e86d4
AmruthaaD <amruthad.xworkz@gmail.com>
2023-11-01 20:11:58 +0530

    constructor example

---
+++ KeyChainStarter.java
@@ -0,0 +1,49 @@
+class KeyChainStarter
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting main in KeyChainStarter..");
+		KeyChain keyChain=new KeyChain();
+		System.out.println("keyChain material :"+keyChain.material);
+		System.out.println("keyChain idealFor :"+keyChain.idealFor);
+		System.out.println("keyChain ratings :"+keyChain.ratings);
+		System.out.println("keyChain weight :"+keyChain.weight);
+		System.out.println("keyChain theme :"+keyChain.theme);
+		
+		KeyChain keyChain1=new KeyChain();
+		System.out.println("keyChain1 material :"+keyChain1.material);
+		System.out.println("keyChain1 idealFor :"+keyChain1.idealFor);
+		System.out.println("keyChain1 ratings :"+keyChain1.ratings);
+		System.out.println("keyChain1 weight :"+keyChain1.weight);
+		System.out.println("keyChain1 theme :"+keyChain1.theme);
+		
+		KeyChain keyChain2=new KeyChain();
+		System.out.println("keyChain2 material :"+keyChain2.material);
+		System.out.println("keyChain2 idealFor :"+keyChain2.idealFor);
+		System.out.println("keyChain2 ratings :"+keyChain2.ratings);
+		System.out.println("keyChain2 weight :"+keyChain2.weight);
+		System.out.println("keyChain2 theme :"+keyChain2.theme);
+		
+		KeyChain keyChain3=new KeyChain();
+		System.out.println("keyChain3 material :"+keyChain3.material);
+		System.out.println("keyChain3 idealFor :"+keyChain3.idealFor);
+		System.out.println("keyChain3 ratings :"+keyChain3.ratings);
+		System.out.println("keyChain3 weight :"+keyChain3.weight);
+		System.out.println("keyChain3 theme :"+keyChain3.theme);
+		
+		KeyChain keyChain4=new KeyChain();
+		System.out.println("keyChain4 material :"+keyChain4.material);
+		System.out.println("keyChain4 idealFor :"+keyChain4.idealFor);
+		System.out.println("keyChain4 ratings :"+keyChain4.ratings);
+		System.out.println("keyChain4 weight :"+keyChain4.weight);
+		System.out.println("keyChain4 theme :"+keyChain4.theme);
+		
+		KeyChain keyChain5=new KeyChain();
+		System.out.println("keyChain5 material :"+keyChain5.material);
+		System.out.println("keyChain5 idealFor :"+keyChain5.idealFor);
+		System.out.println("keyChain5 ratings :"+keyChain5.ratings);
+		System.out.println("keyChain5 weight :"+keyChain5.weight);
+		System.out.println("keyChain5 theme :"+keyChain5.theme);
+		System.out.println("Ending main in KeyChainStarter..");
+	}
+}
