7a23f0b3cbb59347fa35a9a98e8bb25a841a93a9
AmruthaaD <amruthad.xworkz@gmail.com>
2023-11-22 10:20:39 +0530

    using super chaining

---
+++ inheritance/typecasting/TintedGlass.java
@@ -0,0 +1,6 @@
+class TintedGlass extends ToughenedGlass{
+	TintedGlass(double weight,String ownerName,double length,int thickness,GlassType type,String usage,String brand)
+	{
+	super(weight,ownerName,length,thickness,type,usage,brand);
+	}
+}
